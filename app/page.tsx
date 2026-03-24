80451bce248c9cd73079b62cf512345ad8753413
pingandthepong <pingandthepong@gmail.com>
2026-03-24 15:58:36 +0900

    header, footer 사용 위치 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
 export default function Home() {
   return (
     <>
-      <Header />
-
-      <main className="bg-gray-100">main</main>
-
-      <Footer />
+      <main className="flex-1 bg-gray-100">
+        <div className="max-w-7xl  w-full mx-auto px-6 lg:px-8 py-12">Main</div>
+      </main>
     </>
   );
 }
