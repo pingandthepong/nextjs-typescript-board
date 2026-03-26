85c8500c75c58bb2839a97b1447575336b9657ad
pingandthepong <pingandthepong@gmail.com>
2026-03-27 05:54:58 +0900

    fix: py 축소

--- app/components/layout/Container.tsx
+++ app/components/layout/Container.tsx
@@ -1,7 +1,7 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-w-7xl w-full mx-auto">
-      <section className="px-6 lg:px-8 py-12">{children}</section>
+      <section className="px-6 lg:px-8 py-10">{children}</section>
     </div>
   );
 }
