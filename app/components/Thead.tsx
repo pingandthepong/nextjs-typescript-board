d7b108e34e18fcaafe64b84009190f702308aaff
pingandthepong <pingandthepong@gmail.com>
2026-03-31 02:03:15 +0900

    fix: Thead children 타입 지정

--- app/components/Thead.tsx
+++ app/components/Thead.tsx
@@ -1,4 +1,4 @@
-export default function Thead({ children }) {
+export default function Thead({ children }: { children: string }) {
   return (
     <th className="h-12 px-6 text-xs font-semibold text-gray-700 uppercase tracking-wider text-left">
       {children}
